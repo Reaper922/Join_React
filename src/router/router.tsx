b88b9828ecbe097c966f57ee2497ca3b86a1c4c6
Dennis Ammen <dennis.ammen@outlook.com>
2023-11-18 15:35:38 +0100

    Added router

---
+++ src/router/router.tsx
@@ -0,0 +1,50 @@
+import { createBrowserRouter } from "react-router-dom";
+
+import Authentication from "../pages/authentication/authentication.component";
+import AppLayout from "../layouts/app-layout/app-layout.component";
+import Board from "../pages/board/board.component";
+import Contacts from "../pages/contacts/contacts.component";
+import Error from "../pages/error/error.component";
+import LegalNotice from "../pages/legal-notice/legal-notice.component";
+import PrivacyPolicy from "../pages/privacy-policy/privacy-policy.component";
+import Summary from "../pages/summary/summary.component";
+import Task from "../pages/task/task.component";
+
+const appRoutes = [
+  {
+    path: "/",
+    element: <Authentication />,
+    errorElement: <Error />,
+  },
+  {
+    element: <AppLayout />,
+    children: [
+      {
+        path: "/summary",
+        element: <Summary />,
+      },
+      {
+        path: "/board",
+        element: <Board />,
+      },
+      {
+        path: "/task",
+        element: <Task />,
+      },
+      {
+        path: "/contacts",
+        element: <Contacts />,
+      },
+      {
+        path: "/privacy-policy",
+        element: <PrivacyPolicy />,
+      },
+      {
+        path: "/legal-notice",
+        element: <LegalNotice />,
+      },
+    ],
+  },
+];
+
+export const router = createBrowserRouter(appRoutes);
