6f1929867434ed106c5f7662d712f2d5ff91bc67
Dennis Ammen <dennis.ammen@outlook.com>
2023-11-18 15:41:55 +0100

    Added profile-icon component

---
+++ src/components/profile-icon/profile-icon.component.tsx
@@ -0,0 +1,11 @@
+import styles from "./profile-icon.module.scss";
+
+const ProfileIcon = () => {
+  return (
+    <div className={styles.profileIcon}>
+      <span>DA</span>
+    </div>
+  );
+};
+
+export default ProfileIcon;
