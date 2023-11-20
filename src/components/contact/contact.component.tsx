9baf4b06d5a3b1aa9fb181408f705f4aad487dac
Dennis Ammen <dennis.ammen@outlook.com>
2023-11-20 21:31:34 +0100

    Added contact component

---
+++ src/components/contact/contact.component.tsx
@@ -0,0 +1,40 @@
+import { SetStateAction } from "react";
+import styles from "./contact.module.scss";
+
+type ContactProps = {
+  contact: {
+    id: number;
+    firstname: string;
+    lastname: string;
+    email: string;
+  };
+  activeContact: number | null;
+  setContact: React.Dispatch<SetStateAction<number | null>>;
+};
+
+const Contact = ({ contact, activeContact, setContact }: ContactProps) => {
+  const { id, firstname, lastname, email } = contact;
+  const initials = (firstname.charAt(0) + lastname.charAt(0)).toUpperCase();
+
+  return (
+    <div
+      className={[
+        styles.contact,
+        id === activeContact ? styles.active : "",
+      ].join(" ")}
+      onClick={() => setContact(id)}
+    >
+      <div className={styles.bubble}>
+        <span>{initials}</span>
+      </div>
+      <div className={styles.details}>
+        <div>
+          {lastname}, {firstname}
+        </div>
+        <div>{email}</div>
+      </div>
+    </div>
+  );
+};
+
+export default Contact;
