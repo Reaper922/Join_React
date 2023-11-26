738e2d21928bd1f7df7782e45878a99a77ae085d
Dennis Ammen <dennis.ammen@outlook.com>
2023-11-26 18:40:01 +0100

    Added summary component placeholder

---
+++ src/pages/summary/summary.component.tsx
@@ -0,0 +1,12 @@
+import { Link } from "react-router-dom";
+
+const Summary = () => {
+  return (
+    <>
+      <h1>Summary</h1>
+      <Link to={"/"}>Logout</Link>
+    </>
+  );
+};
+
+export default Summary;
