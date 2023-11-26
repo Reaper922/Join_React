19e5bca61939bf8158640e7bdac9fde2a324cd87
Dennis Ammen <dennis.ammen@outlook.com>
2023-11-26 15:36:06 +0100

    Refactored nav-link to display svg's

--- src/components/nav-link/nav-link.component.tsx
+++ src/components/nav-link/nav-link.component.tsx
@@ -1,21 +1,35 @@
-import { Link } from "react-router-dom";
+import React from "react";
 
-// import Logo from "@assets/icons/summary.svg?react";
+import { Link } from "react-router-dom";
 
 import styles from "./nav-link.module.scss";
 
-type navLinkProps = {
+type NavLinkProps = {
   name: string;
   path: string;
-  icon?: string;
+  active: boolean;
+  children?: React.ReactNode;
 };
 
-const NavLink = ({ name, path, icon }: navLinkProps) => {
+const NavLink = ({ name, path, active, children }: NavLinkProps) => {
   return (
-    <Link to={path} className={styles.navLink} draggable="false">
-      {icon && <img src={icon} alt={`${name} Icon`} draggable="false" />}
-      <span>{name}</span>
-    </Link>
+    <>
+      <Link
+        to={path}
+        className={[styles.navLink, active && styles.active].join(" ")}
+        draggable="false"
+      >
+        {children &&
+          React.Children.map(children, (child) =>
+            child
+              ? React.cloneElement(child as React.ReactElement, {
+                  className: styles.svg,
+                })
+              : null
+          )}
+        <span>{name}</span>
+      </Link>
+    </>
   );
 };
 
