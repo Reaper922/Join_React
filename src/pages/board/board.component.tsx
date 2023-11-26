b5aa1db609144d5e4986106d9e7b722063ef707e
Dennis Ammen <dennis.ammen@outlook.com>
2023-11-26 18:39:06 +0100

    Added board component placeholder

---
+++ src/pages/board/board.component.tsx
@@ -0,0 +1,12 @@
+import { Link } from "react-router-dom";
+
+const Board = () => {
+  return (
+    <>
+      <h1>Board</h1>
+      <Link to={"/"}>Logout</Link>
+    </>
+  );
+};
+
+export default Board;
