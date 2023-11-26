35dfbca2a087eedebcc274bd12b5ff8120dcee64
Dennis Ammen <dennis.ammen@outlook.com>
2023-11-26 15:33:01 +0100

    Added link to help page

--- src/components/header/header.component.tsx
+++ src/components/header/header.component.tsx
@@ -1,3 +1,5 @@
+import { Link } from "react-router-dom";
+
 import helpIcon from "@assets/icons/help.svg";
 import logo from "@assets/img/logo.svg";
 import ProfileIcon from "../profile-icon/profile-icon.component";
@@ -10,7 +12,9 @@ const Header = () => {
       <img src={logo} alt="Logo" className={styles.logo} draggable="false" />
       <div className={styles.headerContainer}>
         <span>Kanban Project Management Tool</span>
-        <img src={helpIcon} alt="Help" draggable="false" />
+        <Link to={"/help"} draggable="false">
+          <img src={helpIcon} alt="Help" draggable="false" />
+        </Link>
         <ProfileIcon />
       </div>
     </header>
