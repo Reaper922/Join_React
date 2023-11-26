9f56d9c5835c8cee7e52dd7274cb4fbc69cea221
Dennis Ammen <dennis.ammen@outlook.com>
2023-11-26 18:40:11 +0100

    Added task component placeholder

---
+++ src/pages/task/task.component.tsx
@@ -0,0 +1,12 @@
+import { Link } from "react-router-dom";
+
+const Task = () => {
+  return (
+    <>
+      <h1>Task</h1>
+      <Link to={"/"}>Logout</Link>
+    </>
+  );
+};
+
+export default Task;
