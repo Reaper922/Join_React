daf9e5763b4534de5f72e236c613ca47fc15861d
Dennis Ammen <dennis.ammen@outlook.com>
2023-11-26 18:39:36 +0100

    Added legal notice component placeholder

---
+++ src/pages/legal-notice/legal-notice.component.tsx
@@ -0,0 +1,12 @@
+import { Link } from "react-router-dom";
+
+const LegalNotice = () => {
+  return (
+    <>
+      <h1>Legal Notice</h1>
+      <Link to={"/"}>Logout</Link>
+    </>
+  );
+};
+
+export default LegalNotice;
