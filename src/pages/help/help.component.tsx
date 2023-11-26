b080b8b2e5035c5577745bdf2d4481a9347a8565
Dennis Ammen <dennis.ammen@outlook.com>
2023-11-26 18:39:20 +0100

    Added help component placeholder

---
+++ src/pages/help/help.component.tsx
@@ -0,0 +1,12 @@
+import { Link } from "react-router-dom";
+
+const Help = () => {
+  return (
+    <>
+      <h1>Help</h1>
+      <Link to={"/"}>Logout</Link>
+    </>
+  );
+};
+
+export default Help;
