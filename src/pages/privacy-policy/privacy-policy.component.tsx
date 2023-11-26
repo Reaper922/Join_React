f12c624d1073dd1ef66519109647c9695ff2fa88
Dennis Ammen <dennis.ammen@outlook.com>
2023-11-26 18:39:53 +0100

    Added privacy policy component placeholder

---
+++ src/pages/privacy-policy/privacy-policy.component.tsx
@@ -0,0 +1,12 @@
+import { Link } from "react-router-dom";
+
+const PrivacyPolicy = () => {
+  return (
+    <>
+      <h1>Privacy Policy</h1>
+      <Link to={"/"}>Logout</Link>
+    </>
+  );
+};
+
+export default PrivacyPolicy;
