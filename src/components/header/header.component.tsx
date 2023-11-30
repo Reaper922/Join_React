b40c341969d3e586b30fd95a12d7a1f12746dd5d
Dennis Ammen <dennis.ammen@outlook.com>
2023-11-30 19:03:15 +0100

    Changed header logo to svg component

--- src/components/header/header.component.tsx
+++ src/components/header/header.component.tsx
@@ -1,7 +1,7 @@
 import { Link } from "react-router-dom";
 
 import helpIcon from "@assets/icons/help.svg";
-import logo from "@assets/img/logo.svg";
+import Logo from "@assets/img/logo.svg?react";
 import ProfileIcon from "../profile-icon/profile-icon.component";
 
 import styles from "./header.module.scss";
@@ -9,7 +9,8 @@ import styles from "./header.module.scss";
 const Header = () => {
   return (
     <header className={styles.mainHeader}>
-      <img src={logo} alt="Logo" className={styles.logo} draggable="false" />
+      <Logo className={styles.logo} />
+      {/* <img src={logo} alt="Logo" className={styles.logo} draggable="false" /> */}
       <div className={styles.headerContainer}>
         <span>Kanban Project Management Tool</span>
         <Link to={"/help"} draggable="false">
