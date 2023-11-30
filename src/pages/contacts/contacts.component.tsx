451fa0ffc19a979e5e01a03c6fe8188a512bfd29
Dennis Ammen <dennis.ammen@outlook.com>
2023-11-30 18:12:22 +0100

    Added contact details and setActiveContact

--- src/pages/contacts/contacts.component.tsx
+++ src/pages/contacts/contacts.component.tsx
@@ -1,40 +1,74 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
-import Contact from "../../components/contact/contact.component";
+import Button from "../../components/button/button.component";
+import ContactEntry from "../../components/contact-entry/contact-entry.component";
+import ContactDetails from "../../components/contact-details/contact-details.component";
 import ContactSeparator from "../../components/contact-separator/contact-separator.component";
+
+import AddContact from "@assets/icons/add_contact.svg?react";
+
+import { Contact } from "../../types/contact.type";
 
 import styles from "./contacts.module.scss";
 
-const contacts = [
+const contacts: Contact[] = [
   {
     id: 1,
-    lastname: "Ammen",
     firstname: "Dennis",
+    lastname: "Ammen",
     email: "dennisammen@gmail.com",
+    phone: "+49 123456789",
   },
   {
     id: 2,
-    lastname: "Müller",
     firstname: "Maria",
+    lastname: "Müller",
     email: "mariamueller@gmail.com",
+    phone: "+49 123965478",
   },
   {
     id: 3,
-    lastname: "Janßen",
     firstname: "Sven",
+    lastname: "Janßen",
     email: "svenjanssen@gmail.com",
+    phone: "+49 1239456780",
   },
 ];
 
 const Contacts = () => {
   const [activeContact, setActiveContact] = useState<number | null>(null);
+  const [displayedContact, setDisplayedContact] = useState<Contact | undefined>(
+    undefined
+  );
+
+  // useEffect(() => {
+  //   const fetchUser = async () => {
+  //     fetch("https://randomuser.me/api/?seed=e69088fab65d2fd9&results=20")
+  //       .then((resp) => resp.json())
+  //       .then((data) => console.log(data));
+  //   };
+
+  //   fetchUser();
+  // }, []);
+
+  useEffect(() => {
+    const contact: Contact | undefined = contacts.find(
+      (cont) => cont.id === activeContact
+    );
+
+    setDisplayedContact(contact);
+  }, [activeContact]);
 
   return (
     <div className={styles.container}>
       <section className={styles.contacts}>
+        <Button style="primary">
+          <span>Add Contact</span>
+          <AddContact />
+        </Button>
         <ContactSeparator letter={"A"} />
         {contacts.map((contact) => (
-          <Contact
+          <ContactEntry
             key={contact.id}
             contact={contact}
             activeContact={activeContact}
@@ -42,7 +76,11 @@ const Contacts = () => {
           />
         ))}
       </section>
-      <section className={styles.details}>Details</section>
+      <section className={styles.details}>
+        {displayedContact && (
+          <ContactDetails displayedContact={displayedContact} />
+        )}
+      </section>
     </div>
   );
 };
